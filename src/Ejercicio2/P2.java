b66293b3ecacd4dc395c0d8ade1d2bd4076b23ee
albcorare <thalbertuswtf@gmail.com>
2018-10-30 22:13:47 +0100

    p2

--- src/Ejercicio2/P2.java
+++ src/Ejercicio2/P2.java
@@ -9,95 +9,76 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class P2 {
+	
 	// Dado un fichero de texto con una fecha escrita en cada línea, genere otro
 	// fichero con
 	// las fechas ordenadas y que estén entre dos fechas dadas.
 
-	public static void listaFechas() throws IOException {
-		Integer i = 0;
-		List<String> ls = new ArrayList<String>();
-		//convertir string a local
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/MM/yyyy");
-		BufferedReader bf2 = new BufferedReader(new FileReader("ficheros/fechas"));
-		String date2 = null;
-		//convert String to LocalDate
-		LocalDate localDate = LocalDate.parse(date2, formatter);
-		while((date2 = bf2.readLine()) != null) {
-			ls.add(date2);
-			FileWriter fichero = null;
-			try {
-
-				fichero = new FileWriter("ficheros/fichero_escriturakjhjjkh.txt");
-
-				// Escribimos linea a linea en el fichero
-				for (String linea : ls) {
-					fichero.write(linea + "\n");
-				}
-
-				fichero.close();
-
-			} catch (Exception ex) {
-				System.out.println("Mensaje de la excepción: " + ex.getMessage());
-			}
-			i++;
-		}
+//	public static void listaFechas() throws IOException {
+//		Integer i = 0;
+//		List<String> ls = new ArrayList<String>();
+//		//convertir string a local
+//		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/MM/yyyy");
+//		BufferedReader bf2 = new BufferedReader(new FileReader("ficheros/fechas"));
+//		String date2 = null;
+//		//convert String to LocalDate
+//		LocalDate localDate = LocalDate.parse(date2, formatter);
+//		while((date2 = bf2.readLine()) != null) {
+//			ls.add(date2);
+//			FileWriter fichero = null;
+//			try {
+//
+//				fichero = new FileWriter("ficheros/fichero_escriturakjhjjkh.txt");
+//
+//				// Escribimos linea a linea en el fichero
+//				for (String linea : ls) {
+//					fichero.write(linea + "\n");
+//				}
+//
+//				fichero.close();
+//
+//			} catch (Exception ex) {
+//				System.out.println("Mensaje de la excepción: " + ex.getMessage());
+//			}
+//			i++;
+//		}
 		
 		//-------------------------------------------------------
 		
 		
-		
-		
-		
-		
-		
-		/*List<String> ls = new ArrayList<String>();
-		List<String> ls2 = new ArrayList<String>();
-		BufferedReader bf = new BufferedReader(new FileReader("ficheros/fechas"));
-		String sCadena;
-		String ruta = "ficheros/nuevo.txt";
-		File archivo = new File(ruta);
-		while ((sCadena = bf.readLine()) != null) {
-			ls.add(sCadena);
-			LocalDate date = LocalDate.parse("2017-02-05");
-			System.out.println("Fechas= " + ls.get(i));
-			i++;
-			archivo.createNewFile();
+		public static String creaFicheros() throws IOException{
+			Integer i = 0;
+			BufferedReader bf = new BufferedReader(new FileReader("ficheros/fechas"));
+			Stream<String> a = bf.lines();
+			String fecha = bf.readLine();
+			String[] parts = fecha.split("/");
 			
+			Integer lineas =  (int) a.count();
+			String nu = "19";
+			Integer numEntero = Integer.parseInt(parts[i]);
 			
-		}
-		
-		return ls;*/
-	}
+			while(i <= parts.length) {
+				
+			}
+			
+			while(i<= lineas) {
+				if(numEntero > 1) {
+					return "Lee";
+				}
+				i++;
+			}
+			return "no lee";
+	    }
+			
 	
+		
 	
 
 	public static void main(String[] args) throws IOException {
-	
-		/*
-		//String[] lineas = { "Uno", "Dos", "Tres", "Cuatro", "Cinco", "Seis", "Siete", "..." };
-		
-		List<String> lineas = new ArrayList<String>();
-		lineas.add("Alberto");
-		lineas.add("a");
-
-		/** FORMA 1 DE ESCRITURA **/
-		/*FileWriter fichero = null;
-		try {
-
-			fichero = new FileWriter("ficheros/fichero_escrituraList.txt");
-
-			// Escribimos linea a linea en el fichero
-			for (String linea : lineas) {
-				fichero.write(linea + "\n");
-			}
-
-			fichero.close();
-
-		} catch (Exception ex) {
-			System.out.println("Mensaje de la excepción: " + ex.getMessage());
-		}*/
+		System.out.println(creaFicheros());
 	}
 
 }
